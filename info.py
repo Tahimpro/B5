56e4440c9eafcb8444cda9010674a677d069aae1
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2025-02-22 18:18:17 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,8 @@
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHANNEL_ID = -100XXXXXXXXX  # Private Telegram channel ID
+
+SKY_MOVIES_URL = "https://skymovieshd.video/"
+CHECK_INTERVAL = 300  # 5 minutes (300 seconds)
+POST_GAP = 180  # 3-minute gap (180 seconds)
